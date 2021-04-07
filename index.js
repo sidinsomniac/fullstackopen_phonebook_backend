ffa08190ae538a4cf172b9be2d64982996ef129a
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-04-07 21:14:16 +0530

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 app.use(express.json());
+
+morgan.token('body', function (req, res) {
+    if (req.method === "POST")
+        return JSON.stringify(req.body);
+    return "";
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let people = [
     {
